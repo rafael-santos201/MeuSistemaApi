d282c3a09fcc0bd091f1596e2e15f28e15536283
rafael <rafael201123r@gmail.com>
2025-06-28 17:11:00 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Class.cs
@@ -0,0 +1,10 @@
+﻿namespace MeuSistemaApi.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Senhahash { get; set; }
+        public string Tipo { get; set; }
+    }
+}
