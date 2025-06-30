bc715326ab5333de899a2291ba993a7cad207d78
rafael <rafael201123r@gmail.com>
2025-06-29 23:57:53 -0300

    corrigindo programa.cs

--- Program.cs
+++ Program.cs
@@ -7,11 +7,9 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllers();
 
-
-var app = builder.Build();
-app.UseCors("AllowAll");
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
@@ -21,12 +19,18 @@ builder.Services.AddCors(options =>
               .AllowAnyHeader();
     });
 });
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.MapControllers();
+
 app.UseHttpsRedirection();
 
 app.Run();
