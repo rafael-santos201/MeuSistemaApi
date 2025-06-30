574670247584677f8269fa0724f0de9433e5fbcb
rafael <rafael201123r@gmail.com>
2025-06-29 22:56:16 -0300

    terminando auhcontroller

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllers();
+
 
 var app = builder.Build();
 
@@ -15,6 +17,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.MapControllers();
 app.UseHttpsRedirection();
 
 app.Run();
